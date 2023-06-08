0cd20e529111e7e7ce5da355ab2f1b9d259c03a4
David Samson <david.andrew.engineer@gmail.com>
2023-06-08 17:03:08 -0400

    work on breaking out data by country

--- test.py
+++ test.py
@@ -2,6 +2,9 @@ from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 import xarray as xr
+import rioxarray as rxr
+from rioxarray.exceptions import NoDataInBounds
+
 
 from regrid import regrid, get_resolution, RegridMethod
 
@@ -12,12 +15,6 @@ import pdb
 """
 [example question]
 How many people will be exposed to extreme heat events (e.g., heatwaves) in the future?
-
-Tasks:
-- define what counts as an extreme heat event
-- get projected population data
-- combine the two and aggregate
-- final plot should be a line graph of predicted people exposed (per year)
 """
 
 def get_population_data() -> xr.Dataset:
@@ -47,36 +44,6 @@ def get_population_data() -> xr.Dataset:
     yearly_data = yearly_data.rename({'decade': 'year'})
     
     return yearly_data
-
-import xesmf as xe
-def test():
-    """
-    How many people will be exposed to extreme heat events (e.g., heatwaves) in the future?
-    """
-    pop = get_population_data()
-    heat = get_heatwave_data()
-
-    # match up the years of the population data and heatwave data
-    pop = pop.isel(year=slice(5,None))
-
-    # regrid heat% to the same resolution as population
-    regridder = xe.Regridder(heat, pop, 'bilinear')
-    heat = regridder(heat)
-
-
-    # CDO regridding not working...
-    # pop_res = get_resolution(pop)
-    # heat_res = get_resolution(heat)
-    # heat = regrid(heat, pop_res, RegridMethod.BICUBIC)
-    # pop = regrid(pop, heat_res, RegridMethod.SUM)
-
-    # threshold heat% and multiply by population
-    heat_exposed = ((heat > 0)['year_heat%'] * pop['population']).sum(dim=['lat', 'lon'])
-
-    # plot the results. This is the number of people per year exposed to at least one heat event (temperature > 35°C)
-    heat_exposed.plot()
-    plt.title('People Exposed to Heatwaves by Year')
-    plt.show()
 
 
 
@@ -114,6 +81,100 @@ def get_heatwave_data() -> xr.Dataset:
 
     return heatwave
 
+
+
+
+
+import xesmf as xe
+def test():
+    """
+    How many people will be exposed to extreme heat events (e.g., heatwaves) in the future?
+    """
+    pop = get_population_data()
+    heat = get_heatwave_data()
+
+    # match up the years of the population data and heatwave data
+    pop = pop.isel(year=slice(5,None))
+
+    # # regrid heat% to the same resolution as population
+    regridder = xe.Regridder(heat, pop, 'bilinear')
+    heat = regridder(heat)
+
+
+    # CDO regridding not working... doesn't support custom time unit I'm using.
+    # pop_res = get_resolution(pop)
+    # heat = regrid(heat, pop_res, RegridMethod.BICUBIC)
+    # -- or --
+    # heat_res = get_resolution(heat)
+    # pop = regrid(pop, heat_res, RegridMethod.SUM)
+
+
+    # threshold heat% and multiply by population
+    geo_heat_exposed = ((heat > 0)['year_heat%'] * pop['population'])
+
+    #split out the results by country
+    country_heat_exposed = split_by_country(geo_heat_exposed)
+    pdb.set_trace()
+
+
+    # plot the results. This is the number of people per year exposed to at least one heat event (temperature > 35°C)
+    heat_exposed = geo_heat_exposed.sum(dim=['lat', 'lon'])
+    heat_exposed.plot()
+    plt.title('People Exposed to Heatwaves by Year')
+    plt.show()
+
+
+import geopandas as gpd
+def split_by_country(data: xr.Dataset, countries:list[str]=None) -> xr.Dataset:
+    """split the data by country"""
+
+    # ensure data has correct coordinate system
+    data = data.rio.write_crs(4326)
+
+    # load country data
+    shapefile = 'gadm_0/gadm36_0.shp'
+    sf = gpd.read_file(shapefile)
+
+
+
+    # debug
+    # countries = ['Ethiopia','South Sudan','Somalia','Kenya', 'Sudan', 'Eritrea', 'Somaliland', 
+    #         'Djibouti','Uganda', 'Rwanda', 'Burundi', 'Tanzania']
+    countries = ['China', 'India']
+
+
+    # get the shapefile rows for the countries we want
+    if countries is None:
+        countries = sf['NAME_0'].unique().tolist()
+
+    countries_shp = sf[sf['NAME_0'].isin(countries)]
+
+    
+    out_countries = []
+    out_data = []
+    
+    for i, gid, country, geometry in countries_shp.itertuples():
+        try:
+            clipped = data.rio.clip([geometry], crs=4326)
+        except NoDataInBounds:
+            #TODO: handle inserting an empty array?
+            continue
+
+        # aggregate 
+        country_heat = clipped.sum(dim=['lat', 'lon'])
+
+        # save to output
+        out_countries.append(country)
+        out_data.append(country_heat)
+
+
+    # combine all the data into one xarray
+    #TODO: this isn't quite right
+    out_data = xr.DataArray(out_data, dims=['country', 'year'], coords={'country': out_countries})
+
+
+    pdb.set_trace()
+    ...
 
 
 if __name__ == '__main__':
