e0b69c7255fbf0354a72c9b6d85d74b1c6e40989
David Samson <david.andrew.engineer@gmail.com>
2023-09-27 13:03:38 -0400

    removing old module structure stuff

--- flowcast/__init__.py
+++
@@ -1,2 +0,0 @@
-# from .pipeline import Pipeline, Variable, Threshold, ThresholdType, RegridType, LongitudeConvention
-# from .spacetime import Resolution, Frequency, inplace_set_longitude_convention
